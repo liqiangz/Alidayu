044710b4aafa686a3cc82ebda94091963ef25e6b
李强 <15255829911@163.com>
2016-06-03 21:19:33 +0800

    alidayu

---
+++ src/Alidayu.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Tim
+ * Date: 2016/3/10
+ * Time: 14:59
+ */
+
+namespace Kollway\Alidayu;
+
+include "TopSdk.php";
+
+class Alidayu {
+
+    private $topclient;
+
+    public function __construct() {
+        $this->topclient = new \TopClient(env('ALIDAYU_APP_KEY'), env('ALIDAYU_SECRETKEY'));
+    }
+
+    public function sendSms($mobile,$smsParams) {
+        
+        $req = new \AlibabaAliqinFcSmsNumSendRequest;
+        $req ->setSmsType( "normal" );
+        $req ->setSmsFreeSignName( env('ALIDAYU_SIGN') );
+        $req ->setSmsParam( json_encode($smsParams));
+        $req ->setRecNum( $mobile );
+        $req ->setSmsTemplateCode( env('ALIDAYU_SMS'));
+        return $this->topclient->execute($req);
+    }
+
+}
